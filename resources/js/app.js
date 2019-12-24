f7c15a9f04c109a257b8bf2c312ff950e0152ae8
anower00 <anower.hasan@gmail.com>
2019-12-24 09:59:20 +0600

    progress bar added in create user form

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,6 +16,14 @@ window.Form = Form;
 Vue.component(HasError.name, HasError);
 Vue.component(AlertError.name, AlertError);
 
+import VueProgressBar from 'vue-progressbar'
+
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    failedColor: 'red',
+    height: '5px'
+});
+
 Vue.use(VueRouter);
 
 let routes = [
